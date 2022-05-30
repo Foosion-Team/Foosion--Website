06ddb0756a0adb96f2cccf9c2cde022caf4b10c4
Martins Ikpe <martinsikpe25@gmail.com>
2022-05-30 20:49:40 +0100

    updated cta sections

--- components/molecules/CTA2.jsx
+++ components/molecules/CTA2.jsx
@@ -9,16 +9,29 @@ background-size:cover;
 const FirstText = styled.h1`
 font: normal normal 900 45px/55px Segoe UI;
 padding-bottom:30px;
+width:70%;
+margin:auto;
 `
-const SecondText = styled.p`
-font: normal normal 400 25px/35px Segoe UI;
-padding-bottom:45px;
-`
+const Button = styled.button`
+margin-right:24px;
+padding: 20px;
+width:50%;
+font-size:25px;
+border-radius:11px;
+background: transparent;
+border: 3px solid black;
+@media (max-width: 600px) {
+width:240px;
+font-size:17px;
+padding: 15px;
+}`
+
 const CTA2 = () => {
   return (
     <Div className='flex justify-center items-center text-center'>
         <div>
             <FirstText>Find Us From Anywhere Around the World</FirstText>
+            <Button>See Locations</Button>
         </div>
     </Div>
   )
